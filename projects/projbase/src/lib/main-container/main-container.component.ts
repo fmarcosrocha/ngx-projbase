d9338340dcdc95a1f5bc0bb40ddeb01e08515e5e
Marks Duarte <marksduarte@gmail.com>
2023-03-01 18:45:24 -0300

    Nomes de métodos e variaveis padronizados em inglês.

--- projects/projbase/src/lib/main-container/main-container.component.ts
+++ projects/projbase/src/lib/main-container/main-container.component.ts
@@ -1,7 +1,7 @@
-import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import {Component, EventEmitter, Input, Output, ViewChild} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Observable} from 'rxjs';
+import {map, shareReplay} from 'rxjs/operators';
 
 @Component({
   selector: 'lib-main-container',
@@ -9,23 +9,25 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class MainContainerComponent {
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
 
-
   @Output() scrollTop = new EventEmitter();
 
-  @ViewChild('drawer', { static: false }) sidenavRef!: any;
-  @ViewChild('ProjBaseMainContent', { static: false }) ProjBaseMainContent!: any;
+  @ViewChild('drawer', {static: false}) sidenavRef!: any;
+  @ViewChild('ProjBaseMainContent', {static: false}) ProjBaseMainContent!: any;
 
   @Input() menus: any[] = [];
   @Input() systemName = 'System Name';
-  @Input() license = `ORG. LTDA - ${new Date().getFullYear()}`;
+  @Input() license = `License - ${new Date().getFullYear()}`;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+  }
+
+
   public onScroll(evt: any): void {
     this.scrollTop.emit(evt.target.scrollTop);
   }
